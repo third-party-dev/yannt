a2d64479c51b8068d651512b88bb9e7da590cd17
chenz <crazychenz@gmail.com>
2025-12-19 10:05:17 +0000

    moved pparse to extern

--- clone-pparse.sh
+++ clone-pparse.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+if [ -e "extern/pparse" ]; then
+  echo "'extern/pparse' already exists. Remove to continue."
+  exit 1
+fi
+
 if [ -z "$ORIGIN" ]; then
   echo "You must set ORIGIN envvar before running."
   exit 1
 fi
 
-git clone $ORIGIN pparse
+mkdir -p extern
+git clone $ORIGIN extern/pparse
