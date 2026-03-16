c5c3cb0f5d4fa4042ae185d4b013ce3ad5ecfba3
chenz <crazychenz@gmail.com>
2026-03-16 08:43:29 +0000

    Removed OBE create-bundle scripts

--- plugins/thirdparty_yannt_naive/create-bundle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir bundles
-FPATH=./bundles/yannt_naive-$(date +%Y%m%d).bundle
-git bundle create ${FPATH} --all
-base64 ${FPATH} > ${FPATH}.base64.txt
-echo "Created: ${FPATH}.base64.txt"
--- plugins/thirdparty_yannt_naive_pytorch/create-bundle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir bundles
-FPATH=./bundles/yannt_naive_pytorch-$(date +%Y%m%d).bundle
-git bundle create ${FPATH} --all
-base64 ${FPATH} > ${FPATH}.base64.txt
-echo "Created: ${FPATH}.base64.txt"
