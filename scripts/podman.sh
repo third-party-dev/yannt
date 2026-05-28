41d6d668f6adaa61a7f2aa282959f13edc2ebfcf
chenz <crazychenz@gmail.com>
2026-05-28 13:35:01 +0100

    fixed parameter handling

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Example: ./scripts/podman.sh yannt-py3.9-podman -- run -ti localhost/yannt-py3.9-podman-wine bash
+
 set -e
 
 export PROJ_PATH=$(realpath $(dirname $0)/..)
 export CONFIG_NAME="$1"
 export CONFIG_PATH="${PROJ_PATH}/configs/env/${CONFIG_NAME}"
+
+echo ARGS: $@
 
 # Purge all arguments up to and including first '--'.
 while [[ "$1" != "--" && "$#" -gt 0 ]]; do
@@ -17,11 +21,13 @@ if [ -z "$CONFIG_NAME" -o "$CONFIG_NAME" = "list" ]; then
     exit 0
 fi
 
-if [ ! -e "${CONFIG_PATH}/config" ]; then
+if [ -e "${CONFIG_PATH}/config" ]; then
+    source ${CONFIG_PATH}/config
+elif [ -e "${CONFIG_PATH}/config.env" ]; then
+    source ${CONFIG_PATH}/config.env
+else
     echo "Could not find config file for ${CONFIG_NAME}"
     exit 1
 fi
 
-source ${CONFIG_PATH}/config
-
-${CRI_BIN} $@
+${CRI_BIN} "$@"
