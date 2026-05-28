b14359ea1496a4a84e9991cfd5e71b018b8c6cf7
chenz <crazychenz@gmail.com>
2026-05-28 05:52:25 +0100

    started new env build process

--- scripts/_/build-venv.sh
+++ scripts/_/build-venv.sh
@@ -29,31 +29,60 @@ export PS1="${PS1_TAG}${PS1:-\$ }"
 # Assuming we've already collected.
 PIP_INST_ARGS=${PIP_INST_ARGS:-"--no-index -f ./cache/pip_pkgs/${PY_VER}"}
 PYTHON=${PYTHON:-python}
+VENV_PYTHON=${VENV_PYTHON:-./cache/venv/${ML_VENV_NAME}/bin/python}
+
+# Ensure virtualenv is available.
+echo INSTALL VIRTUALENV: ${PYTHON} -m pip install --upgrade ${PIP_INST_ARGS} virtualenv
+${PYTHON} -m pip install --upgrade ${PIP_INST_ARGS} virtualenv
 
 mkdir -p ./cache/venv
 if [ ! -e "./cache/venv/${ML_VENV_NAME}" ]; then
   # Note: The following line is the only place we enforce python version for venv.
-  ${PYTHON} -m venv ./cache/venv/${ML_VENV_NAME}
+  
+  echo CREATE VENV: ${PYTHON} -m virtualenv ./cache/venv/${ML_VENV_NAME}
+  ${PYTHON} -m virtualenv ./cache/venv/${ML_VENV_NAME}
   [ $? -ne 0 ] && { echo "Failed to create venv"; exit 1; }
 
   echo "--------------------- Setting Up Base Python Requirements ---------------------"
-  ./cache/venv/${ML_VENV_NAME}/bin/pip install --upgrade \
+  ${VENV_PYTHON} -m pip install --upgrade \
     ${PIP_INST_ARGS} ${PY_REQS_ARGS} ${PY_CONSTRAINTS_ARGS}
 fi
-source ./cache/venv/${ML_VENV_NAME}/bin/activate
-echo HERE4
+#source ./cache/venv/${ML_VENV_NAME}/bin/activate
+
+
 # TODO: These dependencies should be managed by pyproject.toml.
 echo Checking dependencies.
 
 # pip install for each enabled
 mkdir -p ${EXTERN_DIR}
 for pkgpath in ${EXTERN_DIR}/*; do
+
+  echo pkgpath = $pkgpath
+
+  skip=0
+  for blocked in $PKG_SKIP_LIST; do
+    #echo blocked = ${EXTERN_DIR}/$blocked
+    if [ "$pkgpath" = "${EXTERN_DIR}/$blocked" ]; then
+      skip=1
+      break
+    fi
+  done
+  if [ "$skip" = "1" ]; then
+    echo "Skipping $pkgpath."
+    continue
+  fi
+
   if [ -d "$pkgpath" ]; then
-    echo pip install -U ${PIP_INST_ARGS} -e $pkgpath ${PY_CONSTRAINTS_ARGS}
-    pip show $(basename "$pkgpath") &>/dev/null || pip install -U ${PIP_INST_ARGS} \
-      ${PY_CONSTRAINTS_ARGS} -e $pkgpath
+    echo ${VENV_PYTHON} -m pip show $(basename "$pkgpath") \&\>\/dev\/null 
+    #bash -i
+    ${VENV_PYTHON} -m pip show $(basename "$pkgpath")
+    echo \|\| ${VENV_PYTHON} -m pip install -U ${PIP_INST_ARGS} ${PY_CONSTRAINTS_ARGS} -e $pkgpath
+    ${VENV_PYTHON} -m pip show $(basename "$pkgpath") &>/dev/null \
+    || ${VENV_PYTHON} -m pip install -U ${PIP_INST_ARGS} ${PY_CONSTRAINTS_ARGS} -e $pkgpath
   fi
 done
+
+echo STARTING CONTAINER
 
 if [ -z "$NO_SHELL" ]; then
   ./scripts/_/start-venv.sh $@
