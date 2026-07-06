cb662957e09176d9eb85a81eefd2e211a99996fe
chenz <crazychenz@gmail.com>
2026-07-06 11:49:04 +0100

    Using rsync to copy images

--- docs/builders/docusaurus/build-docs.sh
+++ docs/builders/docusaurus/build-docs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-export PROJ_PATH=$(realpath $(dirname $0)/../../..)
-
-OUT_PATH=$PROJ_PATH/outputs/docs/docusaurus
-mkdir -p $OUT_PATH
-
 #npx create-docusaurus@latest docusaurus classic
 #npx docusaurus telemetry disable
 #npm config set update-notifier false
 #npx update-browserslist-db@latest
+
+export PROJ_PATH=$(realpath $(dirname $0)/../../..)
+
+OUT_PATH=$PROJ_PATH/outputs/docs/docusaurus
+mkdir -p $OUT_PATH
 
 pushd $PROJ_PATH/docs/builders/docusaurus
 
@@ -16,5 +16,13 @@ npm_config_update_notifier=false \
 DOCUSAURUS_DISABLE_TELEMETRY=1 \
 BROWSERSLIST_UPDATE_DB=false \
 npm run build -- --out-dir $OUT_PATH
+rsync -a \
+  --include='*/' \
+  --include='*.jpg' \
+  --include='*.JPG' \
+  --include='*.png' \
+  --include='*.PNG' \
+  --exclude='*' \
+  $PROJ_PATH/docs/builders/docusaurus/docs/ $OUT_PATH/docs/
 
 popd
